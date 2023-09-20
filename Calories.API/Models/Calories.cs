47c34ac7e827a245fa39713a0c4a3568ead3509d
Bhuwan Tiwari <500192275@fvtc.edu>
2023-09-20 17:56:18 -0500

    Started the new API

---
+++ Calories.API/Models/Calories.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Calories.API.Models
+{
+    public class Calories
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("UserId")]
+        public string UserId { get; set; } = string.Empty;
+        [Required]
+        public double Calorie { get; set; }
+        [Required]
+        public string CalorieName { get; set; } = string.Empty;
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Calories.API/Models/Dto/CaloriesDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Calories.API.Models.Dto
+{
+    public class CaloriesDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public double Calorie { get; set; }
+        public string CalorieName { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
