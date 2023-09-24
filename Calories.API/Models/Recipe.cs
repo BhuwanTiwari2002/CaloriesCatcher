52a941f53cfebd25a29b52b84e841525dcd6230d
Logan Hagemann <300064819@fvtc.edu>
2023-09-24 11:11:45 -0500

    Frameworked Recipe API

---
+++ Calories.API/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace Calories.API.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        //
+        //public double RecipeCalories { get; set; }
+        //public int Protein { get; set; }
+        //public int Carbs { get; set; }
+        //public int Fats { get; set; }
+    }
+}
