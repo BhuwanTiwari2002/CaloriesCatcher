349cc5fcf3036c0218d21c9b6a19dd0d6ee68c6f
Bhuwan <bhuwantiwari2002@gmail.com>
2023-09-30 13:17:10 -0500

    Added field to the database for the code

--- CaloriesCatcher.API/Models/ResetPasswordRequest.cs
+++ CaloriesCatcher.API/Models/ResetPasswordRequest.cs
@@ -2,9 +2,7 @@ namespace AuthApi.API.Models;
 
 public class PasswordResetRequest
 {
-    public int Id { get; set; }
-    public string UserId { get; set; } // Reference to your user entity.
+    public string Email { get; set; } 
     public string ResetCode { get; set; }
     public string Token { get; set; } = string.Empty;
-    public DateTime ExpiryTime { get; set; }
 }
