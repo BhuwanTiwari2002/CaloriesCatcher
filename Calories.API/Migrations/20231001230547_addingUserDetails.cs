c3466fc2cc4d2a7f5d447cee7e1cc9eb54fb6410
Yeeleng Lor <500145084@fvtc.edu>
2023-10-04 18:58:32 -0500

    Added /Profile to UserSettings

---
+++ Calories.API/Migrations/20231001230547_addingUserDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calories.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingUserDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserDetails",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserDetails",
+                columns: new[] { "Id", "BirthDate", "CalorieId", "DailyCalories", "Gender", "Height", "RecipeFoodId", "UserId", "Weight" },
+                values: new object[] { 1, new DateTime(2023, 9, 24, 11, 41, 23, 434, DateTimeKind.Local).AddTicks(6680), 1, 1, "Male", 100.0, 1, "Test", 100.0 });
+        }
+    }
+}
---
+++ Calories.API/Migrations/20231001231529_addingUsersDetialsTest2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calories.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingUsersDetialsTest2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {          
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsersDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    DailyCalories = table.Column<int>(type: "int", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CalorieId = table.Column<int>(type: "int", nullable: false),
+                    RecipeFoodId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsersDetails", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetUsersDetails");        
+        }
+    }
+}
