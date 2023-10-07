fb7e4ebb8b7c32831a9fc82cdb961de58034c204
Yeeleng Lor <500145084@fvtc.edu>
2023-10-07 17:49:04 -0500

    Added code but still not working

--- CaloriesCatcher.UI/Model/UserDetail.cs
+++ CaloriesCatcher.UI/Model/UserDetail.cs
@@ -6,7 +6,7 @@
         public string UserId { get; set; }
         public int Height { get; set; }
         public int Weight { get; set; }
-        public int DailyCalories { get; set; }
+        public Decimal DailyCalories { get; set; }
         public DateTime BirthDate { get; set; }
         public string Gender { get; set; } = string.Empty;
         public int CalorieId { get; set; }
