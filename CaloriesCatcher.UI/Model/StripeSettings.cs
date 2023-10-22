20634b33acf8329eda0c5ebe3363c3f0d5f64d6c
Bhuwan <bhuwantiwari2002@gmail.com>
2023-10-22 06:54:48 -0500

    Fixed by code

--- CaloriesCatcher.UI/Model/StripeSettings.cs
+++ CaloriesCatcher.UI/Model/StripeSettings.cs
@@ -4,4 +4,7 @@ public class StripeSettings
 {
     public string PublishableKey { get; set; }
     public string SecretKey { get; set; }
+    public string SuccessUrl { get; set; }
+    public string CancelUrl { get; set; }
+
 }
--- CaloriesCatcher.UI/Service/StripePaymentsService.cs
+++ CaloriesCatcher.UI/Service/StripePaymentsService.cs
@@ -2,70 +2,45 @@
 using Microsoft.Extensions.Options;
 using Stripe;
 using Stripe.Checkout;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CaloriesCatcher.UI.Service
 {
     public class StripePaymentsService
     {
         private readonly StripeSettings _stripeSettings;
+        private readonly CustomerService _customerService;
+        private readonly SubscriptionService _subscriptionService;
+        private readonly SessionService _sessionService;
+
         public StripePaymentsService(IOptions<StripeSettings> stripeSettings)
         {
             _stripeSettings = stripeSettings.Value;
             StripeConfiguration.ApiKey = _stripeSettings.SecretKey;
-        }
-        public async Task<Customer> CreateCustomerAsync(string email)
-        {
-            var options = new CustomerCreateOptions
-            {
-                Email = email,
-            };
-            var service = new CustomerService();
-            return await service.CreateAsync(options);
-        }
 
-        public async Task<Stripe.Subscription> CreateSubscriptionAsync(string customerId, string priceId)
-        {
-            try
-            {
-                var options = new SubscriptionCreateOptions
-                {
-                    Customer = customerId,
-                    Items = new List<SubscriptionItemOptions>
-            {
-                new SubscriptionItemOptions
-                {
-                    Price = priceId,
-                    Quantity = 1,
-                },
-            },
-                };
-                var service = new SubscriptionService();
-                return await service.CreateAsync(options);
-            }
-            catch (StripeException ex)
-            {
-                throw ex;
-            }
+            _customerService = new CustomerService();
+            _subscriptionService = new SubscriptionService();
+            _sessionService = new SessionService();
         }
+        
+
         public async Task<string> CreateCheckoutSessionAsync(string priceId)
         {
             var options = new SessionCreateOptions
             {
-                PaymentMethodTypes = new List<string>
-                {
-                    "card",
-                },
+                PaymentMethodTypes = new List<string> { "card" },
                 LineItems = new List<SessionLineItemOptions>
                 {
                     new SessionLineItemOptions
                     {
                         Price = priceId,
-                        Quantity = 1,
-                    },
+                        Quantity = 1
+                    }
                 },
                 Mode = "subscription",
-                SuccessUrl = "https://localhost:7024/payment-success?session_id={CHECKOUT_SESSION_ID}",
-                CancelUrl = "https://localhost:7024/payment-success",
+                SuccessUrl = _stripeSettings.SuccessUrl + "?session_id={CHECKOUT_SESSION_ID}",
+                CancelUrl = _stripeSettings.CancelUrl
             };
             var service = new SessionService();
             var session = await service.CreateAsync(options);
