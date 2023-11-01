0d282cd293769bce609282a71690747336337724
Logan Hagemann <300064819@fvtc.edu>
2023-11-01 17:59:36 -0500

    Fixed RecipeDTO

--- Calories.API/Models/Dto/RecipeDto.cs
+++ Calories.API/Models/Dto/RecipeDto.cs
@@ -4,5 +4,8 @@
     {
         public int RecipeId { get; set; }
         public string RecipeName { get; set;} = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public double Calories { get; set; }
+        public string ImagePath { get; set; } = string.Empty;
     }
 }
