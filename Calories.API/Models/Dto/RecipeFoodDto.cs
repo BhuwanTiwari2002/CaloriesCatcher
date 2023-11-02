c905f3d6ae9c8e6d95365b15bb850da804905a6e
Logan Hagemann <300064819@fvtc.edu>
2023-11-02 16:23:53 -0500

    Test

--- Calories.API/Models/Dto/RecipeFoodDto.cs
+++ Calories.API/Models/Dto/RecipeFoodDto.cs
@@ -4,9 +4,8 @@
     {
         public int RecipeIngredientsId { get; set; }
         public int RecipeId { get; set; }
-        public string IngredientName { get; set; }
+        public string IngredientName { get; set; } = string.Empty;
         public double Quantity { get; set; }
-
         public string UserId { get; set; } = string.Empty;
     }
 }
