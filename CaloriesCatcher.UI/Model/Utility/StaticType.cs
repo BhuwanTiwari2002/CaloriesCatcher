b80111cc0bec94975df61d651ad3c3a90952bfed
Bhuwan Tiwari <93685957+BhuwanTiwari2002@users.noreply.github.com>
2023-12-03 08:27:12 -0600

    Added bug fixes

--- CaloriesCatcher.UI/Model/Utility/StaticType.cs
+++ CaloriesCatcher.UI/Model/Utility/StaticType.cs
@@ -2,7 +2,7 @@
 {
     public class StaticType
     {
-        public static string AuthAPIBase { get; set; } = "https://localhost:7002/";
+        public static string AuthAPIBase { get; set; } = "https://bhupintiwari-001-site1.gtempurl.com/";
         public static string CaloriesAPIBase { get; set; } = "https://localhost:7005/";
         public static string TokenCookie { get; set; } = "JWTToken";
         public static string LoginSession { get; set; } = "UserSession";
